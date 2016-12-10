7829b8a6a067e20b2f1393e3aee37b677175f8f0
SeiyuIsobe <iseiyu@hotmail.co.jp>
2016-12-10 21:19:49 +0900

    add deviceregistor

---
+++ SIoTGateway/DeviceRegister/IDataInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DeviceRegister
+{
+    /// <summary>
+     /// Represents component to create initial data for the system
+     /// </summary>
+    public interface IDataInitializer
+    {
+        void CreateInitialDataIfNeeded();
+        Task<List<DeviceModel>> GetAllDevicesAsync();
+        Task<DeviceModel> UpdateDeviceAsync(DeviceModel device);
+        Task<string> RegistDeviceId(string deviceId);
+    }
+}
---
+++ SIoTGateway/DeviceRegister/SimulatorModule.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Configurations;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Helpers;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Repository;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.BusinessLogic;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceRegister
+{
+    public sealed class SimulatorModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ConfigurationProvider>().As<IConfigurationProvider>().SingleInstance();
+            builder.RegisterType<DeviceLogic>().As<IDeviceLogic>();
+            builder.RegisterType<IotHubRepository>().As<IIotHubRepository>();
+            builder.RegisterType<IoTHubDeviceManager>().As<IIoTHubDeviceManager>();
+            builder.RegisterType<DeviceRulesLogic>().As<IDeviceRulesLogic>();
+            builder.RegisterType<DeviceRegistryRepository>().As<IDeviceRegistryCrudRepository>();
+            builder.RegisterType<DeviceRegistryRepository>().As<IDeviceRegistryListRepository>();
+            builder.RegisterType<DeviceRulesRepository>().As<IDeviceRulesRepository>();
+            builder.RegisterType<SecurityKeyGenerator>().As<ISecurityKeyGenerator>();
+            builder.RegisterType<VirtualDeviceTableStorage>().As<IVirtualDeviceStorage>();
+            builder.RegisterType<ActionMappingLogic>().As<IActionMappingLogic>();
+            builder.RegisterType<ActionMappingRepository>().As<IActionMappingRepository>();
+            builder.RegisterType<ActionLogic>().As<IActionLogic>();
+            builder.RegisterType<DataInitializer>().As<IDataInitializer>();
+            builder.RegisterType<ActionRepository>().As<IActionRepository>();
+            builder.RegisterType<AzureTableStorageClientFactory>().As<IAzureTableStorageClientFactory>();
+            builder.RegisterType<BlobStorageClientFactory>().As<IBlobStorageClientFactory>();
+            builder.RegisterGeneric(typeof(DocumentDBClient<>)).As(typeof(IDocumentDBClient<>));
+        }
+    }
+}
