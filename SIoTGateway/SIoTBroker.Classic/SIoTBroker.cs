7bc1605d8dd5aead9031f0144b0fd305569d5967
SeiyuIsobe <iseiyu@hotmail.co.jp>
2017-01-08 15:33:26 +0900

    ゲートウェイのクラシック化

---
+++ SIoTGateway/SIoTBroker.Classic/SIoTBroker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace SIoTBroker
+{
+    public sealed class SIoTBroker
+    {
+        private MqttBroker _broker = null;
+
+        public void Start()
+        {
+            if (null == _broker)
+            {
+                _broker = new MqttBroker();
+                _broker.Start();
+            }
+            else
+            {
+                _broker.Stop();
+                _broker = null;
+            }
+        }
+
+        public void Stop()
+        {
+            if (null != _broker) Start();
+        }
+    }
+}
